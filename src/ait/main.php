ba752665e3b2bfb43f676cbf4a6985bbb23c27f0
dexen deVries <dexen.devries@gmail.com>
2021-02-21 18:12:44 +0100

    bugfix

--- src/ait/main.php
+++ src/ait/main.php
@@ -10,7 +10,7 @@ function showHelp()
 	echo "usage: php ait.php CONFIG_FILE SERVER_URL UPLOAD_SOURCE ...\n";
 }
 
-if (in_array($argv[0]??null, [ '-h', '--help']))
+if (in_array($argv[1]??null, [ '-h', '--help']))
 	die(showHelp());
 
 $Config = new Config($argv[1]);
