058e4ad6c0b9e14df67ae0d311ab1494aceea5aa
dexen deVries <dexen.devries@gmail.com>
2021-02-21 18:36:42 +0100

    pass through

--- src/lib.php
+++ src/lib.php
@@ -2,4 +2,4 @@
 
 function H($str) { return htmlspecialchars($str); }
 function td(...$a) { echo '<pre>'; foreach ($a as $v) echo H(print_r($v, true)); die('td()'); }
-function tp(...$a) { echo '<pre>'; foreach ($a as $v) echo H(print_r($v, true)); echo "</pre>\ntp()\n"; }
+function tp(...$a) { echo '<pre>'; foreach ($a as $v) echo H(print_r($v, true)); echo "</pre>\ntp()\n"; return $a[0] ?? null; }
