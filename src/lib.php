cc3b93c442ec67aa8a1591d5aa1d259a0367a42a
dexen deVries <dexen.devries@gmail.com>
2021-02-21 18:23:33 +0100

    better output

--- src/lib.php
+++ src/lib.php
@@ -2,4 +2,4 @@
 
 function H($str) { return htmlspecialchars($str); }
 function td(...$a) { echo '<pre>'; foreach ($a as $v) echo H(print_r($v, true)); die('td()'); }
-function tp(...$a) { echo '<pre>'; foreach ($a as $v) echo H(print_r($v, true)); echo '</pre>'; echo 'tp()'; }
+function tp(...$a) { echo '<pre>'; foreach ($a as $v) echo H(print_r($v, true)); echo "</pre>\ntp()\n"; }
