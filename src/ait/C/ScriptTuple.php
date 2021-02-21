983138671b7dfcc07f3804738aacfc17b9f0cda9
dexen deVries <dexen.devries@gmail.com>
2021-02-21 17:46:06 +0100

    user aide: datetime

--- src/ait/C/ScriptTuple.php
+++ src/ait/C/ScriptTuple.php
@@ -4,16 +4,18 @@ class ScriptTuple
 {
 	protected $url;
 	protected $script_name;
+	protected $datetime;
 	protected $password;
 
 	function __construct(array $rcd)
 	{
-		if (count($rcd) !== 3)
-			throw new \LogicException(sprintf('expected a record of 3 items, got %d', count(rcd)));
+		if (count($rcd) !== 4)
+			throw new \LogicException(sprintf('expected a record of 4 items, got %d', count(rcd)));
 
 		$this->url = $rcd[0];
 		$this->script_name = $rcd[1];
-		$this->password = $rcd[2];
+		$this->datetime = $rcd[2];
+		$this->password = $rcd[3];
 	}
 
 	function purgeSecrets()
@@ -24,6 +26,8 @@ class ScriptTuple
 	function url() : string { return $this->url; }
 
 	function scriptName() : string { return $this->script_name; }
+
+	function datetime() : string { return $this->datetime; }
 
 	function pasword() : string
 	{
