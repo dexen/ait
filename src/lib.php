4c1c9a7f23d29b6b19bca9f3f1d589566f75c8e6
dexen deVries <dexen.devries@gmail.com>
2021-02-26 18:41:55 +0100

    nicer debug output

--- src/lib.php
+++ src/lib.php
@@ -2,7 +2,7 @@
 
 function H($str) { return htmlspecialchars($str); }
 function U($str) { return rawurlencode($str); }
-function td(...$a) { echo '<pre>'; foreach ($a as $v) echo H(print_r($v, true)); die('td()'); }
+function td(...$a) { echo '<pre>'; foreach ($a as $v) echo H(print_r($v, true)); die("\n--\ntd()"); }
 function tp(...$a) { echo '<pre>'; foreach ($a as $v) echo H(print_r($v, true)); echo "</pre>\ntp()\n"; return $a[0] ?? null; }
 
 
