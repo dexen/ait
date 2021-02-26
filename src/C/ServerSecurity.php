aed0dae63d94dc0ca348db6b764c38b76ca10be4
dexen deVries <dexen.devries@gmail.com>
2021-02-26 17:41:31 +0100

    expect script address to match the encoded preference

--- src/C/ServerSecurity.php
+++ src/C/ServerSecurity.php
@@ -4,6 +4,7 @@ class ServerSecurity
 {
 	function __construct()
 	{
+		$this->expectInstallationMatches();
 		session_start();
 	}
 	
@@ -22,5 +23,21 @@ class ServerSecurity
 
 		if (password_verify($password, $this->passwordHash()))
 			$_SESSION['_is_logged_in'] = true;
+	}
+
+	function expectInstallationMatches()
+	{
+		if (empty($_SERVER['HTTP_HOST']))
+			throw new \RuntimeException('could not verify host match');
+		if ($_SERVER['HTTP_HOST'] !== private_function_sait_preference_host())
+			throw new \RuntimeException('host does not match script preferences');
+
+		if (empty($_SERVER['REQUEST_URI']))
+			throw new \RuntimeException('could not verify uri match');
+		$a = parse_url($_SERVER['REQUEST_URI']);
+		if (empty($a['path']))
+			throw new \RuntimeException('could not verify uri match');
+		if (rawurldecode($a['path']) !== private_function_sait_preference_uri())
+			throw new \RuntimeException('uri does not match script preferences');
 	}
 }
